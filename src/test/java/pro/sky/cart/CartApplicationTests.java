02fdaf55d13a1ffb876130a9915d60a0e2cb3a6b
shard <alisher.sharipov2@mail.ru>
2023-06-05 02:30:40 +0600

    initial push

---
+++ src/test/java/pro/sky/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
